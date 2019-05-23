78d8ecc46ee1828da38dc806b3e31d8d08c1e044
simokazoo <french.simona@yahoo.com>
2019-05-23 14:13:24 +0200

    Updates

---
+++ precache-manifest.9e49d6dab306066bfbfdaac3e567332e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e5c568e82eafd2eb33bb513ed6332d1",
+    "url": "/personal-list/index.html"
+  },
+  {
+    "revision": "005050f6892031586e31",
+    "url": "/personal-list/static/css/main.eba25275.chunk.css"
+  },
+  {
+    "revision": "3a9e33913cab0720716d",
+    "url": "/personal-list/static/js/2.86a2265c.chunk.js"
+  },
+  {
+    "revision": "005050f6892031586e31",
+    "url": "/personal-list/static/js/main.247768bc.chunk.js"
+  },
+  {
+    "revision": "7b30ce4b02bf0392c8ed",
+    "url": "/personal-list/static/js/runtime~main.a75d9c2f.js"
+  }
+]);
